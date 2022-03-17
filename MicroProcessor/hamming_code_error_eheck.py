83139760830c3253eb3d8290508ad2b9962de210
nzkim1234 <nzkim1234@naver.com>
2022-03-17 23:35:20 +0900

    과제 수정

--- MicroProcessor/hamming_code_error_eheck.py
+++ MicroProcessor/hamming_code_error_eheck.py
@@ -5,6 +5,7 @@ print("해밍코드를 입력하시오.")
 
 bits = list(map(int, map(str, stdin.readline().strip())))
 len_parity_bit = 0
+parity_bit = []
 err = False
 
 while pow(2, len_parity_bit) < len(bits):
@@ -26,10 +27,18 @@ for i in range(len_parity_bit):
                 count_1 += 1
         count += 1
 
+    parity_bit.append(count_1 % 2)
+
     if count_1 % 2 != 0:
         err = True
     
 if err:
-    print('에러발생')
+    parity_bit.reverse()
+    err_bit = int("0b" + "".join(map(str,parity_bit)),2)
+    if bits[err_bit - 1] == 1:
+        bits[err_bit - 1] = 0
+    else:
+        bits[err_bit - 1] = 1
+    print(str(err_bit) + "번째 비트에 에러 발생. 수정된 코드: " + "".join(map(str, bits)))
 else:
     print('에러없음')
